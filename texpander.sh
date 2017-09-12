3b2d28b29824bd96bb4b7631d498fae68ecacc30
Lee Blue <lee@lee.blue>
2017-09-12 11:01:42 -0400

    Get prodcess name before zenity opens
    This dramatically improves the reliability of getting the correct window and process id.

--- texpander.sh
+++ texpander.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# Version: 1.2
-# Release: September 11, 2017
+# Version: 1.2.1
+# Release: September 12, 2017
+
+pid=$(xdotool getwindowfocus getwindowpid)
+proc_name=$(cat /proc/$pid/comm)
 
 base_dir="${HOME}/.texpander/"
 abbrvs=$(ls $base_dir)
+
 name=$(zenity --list --title=Texpander --column=Abbreviations $abbrvs)
 
 path=$base_dir$name
 
 if [[ $name ]]
 then
-  pid=$(xdotool getwindowfocus getwindowpid)
-  proc_name=$(cat /proc/$pid/comm)
-
   if [ -e "$path" ]
   then
     clipboard=$(xclip -selection clipboard -o)
     xclip -selection c -i "$path"
 
-    if [ -n $proc_name ] || [[ $proc_name =~ (termin|nano) ]]
+    if [[ $proc_name =~ (termin|nano) ]]
     then
       xdotool key ctrl+shift+v
     else
